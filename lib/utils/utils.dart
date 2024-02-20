3188834e6c394230a46e0ad240e41e009a2fe486
Sr Software Engieneer <sse@mail.com>
2024-02-20 12:50:35 +0500

    plugins upgrade.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,19 +1,17 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:another_flushbar/flushbar_route.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Utils {
-
-  static double averageRating(List<int> rating)
-  {
+  static double averageRating(List<int> rating) {
     var avgRating = 0;
-    for(int i = 0;i<rating.length; i++){
-      avgRating= avgRating +rating[i];
+    for (int i = 0; i < rating.length; i++) {
+      avgRating = avgRating + rating[i];
     }
-    return double.parse((avgRating/rating.length).toStringAsFixed(1));
+    return double.parse((avgRating / rating.length).toStringAsFixed(1));
   }
+
   //On pressing done from keyboard it should move to the next text field
   static void fieldFocusChange(
     BuildContext context,
