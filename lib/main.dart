2f03d555760f3cf63e5b8f21c9f7321ff491c2cb
Sr Software Engieneer <sse@mail.com>
2024-02-20 11:32:32 +0500

    API call manager integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:now_in_flutter/utils/routes/routes.dart';
+import 'package:now_in_flutter/utils/routes/routes_name.dart';
+import 'package:now_in_flutter/view_model/auth_view_model.dart';
+import 'package:now_in_flutter/view_model/user_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthViewModel()),
+        ChangeNotifierProvider(create: (_) => UserViewModel()),
+      ],
+      child: MaterialApp(
+        title: 'Now In Flutter',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
         ),
+        initialRoute: RoutesName.splash,
+        onGenerateRoute: Routes.generateRoute,
+
+//home: const LoginScreen(),
       ),
     );
   }
